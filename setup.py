e474ac96a97917da946baee95f23c72d638c0a27
Sergey Tikhonov <zimbler@gmail.com>
2014-09-25 14:51:09 +0400

    support aiohttp 0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from shutil import copy
 from dvasya import VERSION
 
-install_requires=['aiohttp>=0.8,<0.9']
+install_requires=['aiohttp>=0.9.1,<0.10']
 
 
 PY_VER = sys.version_info
