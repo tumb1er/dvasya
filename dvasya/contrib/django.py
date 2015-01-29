8b26bed5a6f47d901173b08a65cc8cb13321a700
Sergey Tikhonov <zimbler@gmail.com>
2015-01-29 13:51:15 +0300

    #8 django contrib

--- dvasya/contrib/django.py
+++ dvasya/contrib/django.py
@@ -2,108 +2,122 @@
 
 # $Id: $
 import asyncio
-from inspect import isgenerator
-from aiohttp.web import Response, Request, StreamResponse
+import cgi
+import codecs
+
+from aiohttp.web import Request, Response, StreamResponse, FileField
 from django.http import StreamingHttpResponse
 from django.http.request import HttpRequest
-from dvasya.urls import RegexMatchInfo, UrlResolver
+
+from dvasya.cookies import parse_cookie
 
 
-class MixedRequest(Request, HttpRequest):
-    """ United interface for aiohttp and django Requests."""
+class DjangoRequestProxy(HttpRequest):
+    """ Proxy for aiohttp.web.Request
 
-    def init(self):
-        self.encoding = None
-        self.user = None
-        self.COOKIES, self.META, self.FILES = {}, {}, {}
-        for k, v in self.headers.items():
-            self.META[k.upper()] = v
-
-    @classmethod
-    def combine(cls, request):
-        """ Injects HttpRequest class to request parent classes and initializes
-        HttpRequest object attributes for Request instance"""
-        request.__class__ = cls
-        request.init()
-        return request
-
-
-class MixedStreamingResponse(StreamResponse):
-
-    def __init__(self, *, body=None, status=200,
-                 reason=None, headers=None,
-                 text=None, content_type=None):
-        super().__init__(status=status, reason=reason)
-        self._body = body
-        if headers is not None:
-            self.headers.extend(headers)
-        if content_type:
-            self.content_type = content_type
-
-    def write_eof(self):
-        if self._body:
-            for chunk in self._body:
-                self.write(chunk)
-        return super().write_eof()
-
-
-class DjangoMatchInfo(RegexMatchInfo):
-    """ aiohttp match info to work with django HttpRequest and HttpResponse.
-
-    Transforms aiohttp.web.Request object to django HttpRequest, and constructs
-    aiohttp.web.Response if result of request handling is django HttpResponse.
+    Should be used as Django HttpRequest.
     """
 
-    @staticmethod
-    def subclass_request(request: Request) -> MixedRequest:
-        """ Patches request class to unite both aiohttp and django Request
-        interfaces. """
-        return MixedRequest.combine(request)
+    def __init__(self, request: Request) -> HttpRequest:
+        super().__init__()
+        self.__request = request
+        self.encoding = self._parse_encoding(request)
+        self.method = request._method
+        self.GET = request.GET
+        self.COOKIES = parse_cookie(request.headers.get("COOKIE", ""))
+        self._init_meta(request)
 
-    @staticmethod
-    def subclass_response(response):
-        """ Приводит ответ View к классу aiohttp.web.Response. """
-        if hasattr(response, 'render'):
-            response.render()
-        headers = dict(v for k, v in response._headers.items()
-                           if k != 'content-type')
+    def post(self):
+        # FIXME: for now (aiohttp==1.14.x) payload is parsed by cgi parser
+        task = asyncio.Task(self.__request.post())
+        task.add_done_callback(self._process_payload)
+        return task
 
-        if isinstance(response, StreamingHttpResponse):
-            content = response.streaming_content
-            return MixedStreamingResponse(body=content,
-                            status=response.status_code,
-                            headers=headers,
-                            content_type=response['Content-Type'])
-        else:
-            content = response.content
-            return Response(body=content,
-                            status=response.status_code,
-                            headers=headers,
-                            content_type=response['Content-Type'])
+    def _process_payload(self, task):
+        data = task.result()
+        for key, value in data.items():
+            if isinstance(value, FileField):
+                self.FILES[key] = self._file_field(value)
+            else:
+                self.POST[key] = value
+        return data
 
-    @property
-    def handler(self):
-        return self._wrapper
+    def _file_field(self, value):
+        # FIXME: aiohttp.FileField is not compatible with django one's
+        return value
+
+    def _parse_encoding(self, request):
+        content_type = request.headers.get('CONTENT-TYPE', '')
+        _, content_params = cgi.parse_header(content_type)
+        if 'charset' in content_params:
+            try:
+                codecs.lookup(content_params['charset'])
+            except LookupError:
+                pass
+            else:
+                return content_params['charset']
+        return None
+
+    def _init_meta(self, request):
+        for k, v in request.headers.items():
+            self.META[k.upper().replace('-', '_')] = v
+        # FIXME: Remote_addr, remote_port
+
+
+@asyncio.coroutine
+def django_middleware_factory(app, handler):
+    """ aiohttp middleware for transforming request and response
+    between aiohttp and Django.
+    """
 
     @asyncio.coroutine
-    def _wrapper(self, request):
+    def middleware(request):
 
-        if not isinstance(request, HttpRequest):
-            request = self.subclass_request(request)
+        django_request = DjangoRequestProxy(request)
 
-        ret = self._handler(request, *self._args, **self._kwargs)
+        response = handler(django_request)
+        if asyncio.iscoroutine(response):
+            response = yield from response
 
-        if isgenerator(ret):
-            ret = yield from ret
-
-        response = ret
-        if not isinstance(response, Response):
-            response = self.subclass_response(response)
-
-        return response
+        if isinstance(response, StreamingHttpResponse):
+            response_class = StreamingResponseProxy
+        else:
+            response_class = ResponseProxy
+        return response_class(response)
+    return middleware
 
 
-class DjangoCompatUrlResolver(UrlResolver):
-    match_info_class = DjangoMatchInfo
+class ResponseProxy(Response):
+    """ Proxy for Django HttpResponse.
 
+    Should be used as aiohttp.web.Response.
+    """
+    def __init__(self, response):
+        if hasattr(response, 'render') and callable(response.render):
+            response.render()
+        super(ResponseProxy, self).__init__(
+            body=response.content,
+            status=response.status_code,
+            headers=response,
+            content_type=response.get('CONTENT-TYPE')
+
+        )
+        self.__response = response
+
+
+class StreamingResponseProxy(StreamResponse):
+    """ Proxy for Django StreamResponse
+
+    Should be used as aiohttp.web.Response.
+    """
+
+    def __init__(self, response: StreamingHttpResponse)-> StreamResponse:
+        super().__init__(status=response.status_code)
+        self.__response = response
+
+    def write_eof(self):
+        if self.__response.streaming_content:
+            for chunk in self.__response.streaming_content:
+                self.write(chunk)
+        return super().write_eof()
 
