031885ac76cb3f28e226dd6ed8e235e4b10fe116
Sergey Tikhonov <zimbler@gmail.com>
2015-01-29 14:09:43 +0300

    #8 test views updated

--- testapp/views.py
+++ testapp/views.py
@@ -4,8 +4,6 @@
 import json
 import asyncio
 from aiohttp.web import Response
-
-from dvasya.response import HttpResponse
 from dvasya.views import View
 
 
@@ -18,12 +16,12 @@ class DefaultView(View):
             request.version[0],
             request.version[1]
         )
-        return HttpResponse(body, 200)
+        return Response(body=body, status=200)
 
 
 def dump_args_view(request, *args, **kwargs):
     body = u"<h3>Arguments: {}</h3><h3>Keywords: {}</h3>".format(args, kwargs)
-    return HttpResponse(body, 200)
+    return Response(body=body, status=200)
 
 def function_view(*args, **kwargs):
     return patched_function_view(*args, **kwargs)
@@ -106,15 +104,10 @@ class ClassBasedView(View):
 class TestView(View):
     def get(self, request, *args, **kwargs):
         agent = request.headers.get('user-agent', '')
-        return HttpResponse("<H2>Agent: {}</H2>".format(agent))
+        return Response(text="<H2>Agent: {}</H2>".format(agent))
 
     @asyncio.coroutine
     def post(self, request, *args, **kwargs):
         post = request.POST
         files = request.FILES
-        return HttpResponse("<H2>POST: </H2>\n{}\n<H2>FILES: </H2>\n{}\n".format(post, files))
-
-    def process_payload(self):
-        if int(self.request.headers['Content-Length']) > 10000000:
-            self.request.transport.close()
-        return super().process_payload()
+        return Response(text="<H2>POST: </H2>\n{}\n<H2>FILES: </H2>\n{}\n".format(post, files))
