e0ae18daac264cee087176513365c829d280d0f8
Sergey Tikhonov <zimbler@gmail.com>
2015-02-03 15:11:32 +0300

    separate django tests

--- tests.py
+++ tests.py
@@ -302,33 +302,3 @@ class DvasyaRequestParserTestCase(DvasyaServerTestCaseBase):
 
     def testNotFinishedOctetStream(self):
         self.skipTest("fixme")
-
-
-class DjangoTestCase(DvasyaTestCase):
-    root_urlconf = 'testapp.django_compat.urls'
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapp.settings')
-        from dvasya.contrib.django import DjangoRequestProxyMiddleware
-
-        cls.middlewares = [DjangoRequestProxyMiddleware.factory]
-
-    def testRequestEncoding(self):
-        url = '/rest/'
-        headers = {
-            'Content-Type': 'text/plain; charset=cp1251'
-        }
-        data = {"ok": True}
-        from rest_framework.response import Response
-
-        with mock.patch('testapp.django_compat.views.SampleView.get',
-                        return_value=Response(data)) as p:
-            response = self.client.get(url, headers=headers)
-            self.assertEqual(response.text, json.dumps(data).replace(' ', ''))
-        request = p.call_args[0][0]
-        self.assertEqual(request.encoding, "cp1251")
-
-    def testDjangoRestFrameworkPost(self):
-        self.skipTest("FIXME")
