361ecb29da776aa6028c54f23963ece55e38d5d9
Sergey Tikhonov <zimbler@gmail.com>
2015-02-03 13:02:59 +0300

    #10 django-style middlewares

--- dvasya/middleware.py
+++ dvasya/middleware.py
@@ -2,7 +2,7 @@
 
 # $Id: $
 import asyncio
-from aiohttp.web import Response
+from aiohttp.web import Response, StreamResponse
 from dvasya.conf import settings
 from dvasya.request import DvasyaRequestProxy
 from dvasya.response import HttpInternalError
@@ -19,6 +19,71 @@ class DvasyaMiddlewareBase(object):
     def __init__(self, app, handler):
         self.app = app
         self.handler = handler
+
+    @asyncio.coroutine
+    def __call__(self, request):
+        try:
+            # process request
+            ret = self.process_request(request)
+            if asyncio.iscoroutine(ret):
+                ret = yield from ret
+            # if process_request() returned none, call next middleware
+            if ret is None:
+                ret = self.handler(request)
+                if asyncio.iscoroutine(ret):
+                    ret = yield from ret
+            # passing response to process_response
+            response = ret
+        except Exception as e:
+            # process exception
+            ret = self.process_exception(request, e)
+            if asyncio.iscoroutine(ret):
+                ret = yield from ret
+            if ret is None:
+                if settings.DEBUG:
+                    return HttpInternalError(e)
+                raise
+            response = ret
+        # if has response from next middleware, or process_request, or
+        # process_exception, call process_response(response)
+
+        ret = self.process_response(request, response)
+        if asyncio.iscoroutine(ret):
+            ret = yield from ret
+        if not isinstance(ret, StreamResponse):
+            raise RuntimeError(
+                "%s.process_response() must return a StreamResponse"
+                % self.__class__.__name__)
+        return ret
+
+    def process_request(self, request):
+        """ called before handler.
+
+        :param request: aiohttp.web.Request
+        :return aiohttp.web.Response or None
+        """
+        return None
+
+    def process_exception(self, request, exc):
+        """ called when exception occured in process_request or in handler.
+
+        :param request: aiohttp.web.Request
+        :param exc: Exception
+        :return aiohttp.web.Response or None
+        """
+        return None
+
+    def process_response(self, request, response):
+        """ called after receiving response from process_request, handler
+        or process_exception
+
+        :param request: aiohttp.web.Request
+        :param response: aiohttp.web.Response
+        :return aiohttp.web.Response
+
+        MUST return aiohttp.web.Response object!
+        """
+        return response
 
 
 class RequestProxyMiddleware(DvasyaMiddlewareBase):
@@ -47,11 +112,6 @@ class RequestProxyMiddleware(DvasyaMiddlewareBase):
         else:
             # noinspection PyArgumentList
             return self.response_class(result)
-
-    def format_traceback(self, exc):
-        import traceback
-        trace = traceback.format_exc()
-        return "<h2>"
 
 
 def load_middlewares():
